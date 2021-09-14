dd60ed073e7a5856a81480c139d5c1608dce7c71
Bulmaro Arellano <tomaro@outlook.es>
2021-09-13 20:14:43 -0500

    Blog update

--- src/Pages/PostDetail/index.js
+++ src/Pages/PostDetail/index.js
@@ -3,15 +3,17 @@ import {
     useParams
 } from 'react-router-dom'
 import { Card, Row, Col, CardBody, CardTitle, CardText } from 'reactstrap'
+ 
 
 const PostDetail = props => {
     const { id } = useParams()
+    console.log(id);
     return(
         <Row>
             <Col xs="12">
                 <Card>
                     <CardBody>
-                        <CardTitle >Post seleccionado { id }</CardTitle>
+                        <CardTitle >{ id }</CardTitle>
                         <CardText>Descripcion del Producto seleccionado</CardText>
                     </CardBody>
                 </Card>
